fd843819c6615f7306b0816e786077e1ca3acf8b
bansalmohit123 <149596867+bansalmohit123@users.noreply.github.com>
2024-08-04 17:20:13 +0530

    mongo db connect

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
+const app = express();
+mongoose
+  .connect("mongodb://mohit:mypassword@mongo:27017/?authSource=admin")
+  .then(() => console.log("successfully connected to db"))
+  .catch((e) => console.log(e));
+const port = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("<h2> hi there!!!!!!</h2>");
+});
 
-const app =express();
-
-const port =process.env.PORT||3000;
-app.get("/",(req,res)=>{
-    res.send("<h2> hi there!!!!!!</h2>");
-})
-
-app.listen(port ,()=>console.log(`listening on port ${port}`));
-
+app.listen(port, "0.0.0.0", () => console.log(`listening on port ${port}`));
